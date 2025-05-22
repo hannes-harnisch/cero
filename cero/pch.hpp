1d0bd44c89f45e0fed2888fbc914304f34c340b3
Hannes Harnisch <hannes.harnisch@gmx.net>
2025-05-22 11:35:59 +0200

    Initial commit

---
+++ cero/pch.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <cstdint>
+#include <expected>
+#include <filesystem>
+#include <optional>
+#include <queue>
+#include <source_location>
+#include <span>
+#include <string>
+#include <string_view>
+#include <system_error>
+#include <utility>
+#include <vector>
+
+#include <fmt/args.h>
+#include <fmt/core.h>
+#include <fmt/format.h>
