4b5f128efc6ee335319bda8f70ae07f4d595277e
Hannes Harnisch <hannes.harnisch@gmx.net>
2025-05-24 03:12:32 +0200

    Add reporter interface

--- cero/syntax/lexer.hpp
+++ cero/syntax/lexer.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include "cero/report/reporter.hpp"
 #include "cero/syntax/source.hpp"
 #include "cero/syntax/token_list.hpp"
 
 namespace cero {
 
 /// Performs lexical analysis on a single source input.
-TokenList run_lexer(const SourceView& source, bool emit_comments, uint8_t tab_size = default_tab_size);
+TokenList run_lexer(const SourceView& source, Reporter& reporter, bool emit_comments, uint8_t tab_size = default_tab_size);
 
 } // namespace cero
