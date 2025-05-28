62bde8cc794e4d99894ff57e84269760e813116a
Hannes Harnisch <hannes.harnisch@gmx.net>
2025-05-28 02:09:07 +0200

    Implement keywords in lexer

--- tests/syntax/lexer_results.cpp
+++ tests/syntax/lexer_results.cpp
@@ -1,29 +1,23 @@
 #include "common/test.hpp"
 #include "common/test_reporter.hpp"
+#include "syntax/common.hpp"
 
 #include <cero/syntax/lexer.hpp>
 
+using enum cero::TokenKind;
+
 namespace tests {
-
-namespace {
-
-void expect_tokens(const cero::TokenList& tokens, std::span<const cero::Token> expected) {
-	CHECK_EQ(tokens.get_array().size(), expected.size());
-	CHECK(std::ranges::equal(tokens.get_array(), expected));
-}
-
-} // namespace
 
 CERO_TEST(lexer_empty_source) {
 	cero::SourceView source = make_test_source("");
 
 	TestReporter r;
-	cero::TokenList tokens = cero::run_lexer(source, r, true);
+	cero::TokenList tokens = cero::run_lexer(source, r, cero::LexerFlags::emit_comments);
 
 	cero::Token expected[] = {
-		{cero::TokenKind::end_of_file, 0, 0},
+		{end_of_file, 0, 0},
 	};
-	expect_tokens(tokens, expected);
+	check_tokens_equal(tokens, expected);
 }
 
 } // namespace tests
