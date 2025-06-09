ada77936a999cab48caabe2723c1c6dba5f14460
Hannes Harnisch <hannes.harnisch@gmx.net>
2025-06-09 16:18:05 +0200

    Add memory-mapped file sources

---
+++ tests/compile/build_results.cpp
@@ -0,0 +1,24 @@
+#include "common/test.hpp"
+#include "common/test_reporter.hpp"
+
+#include <cero/compile/build.hpp>
+
+namespace tests {
+
+CERO_TEST(build_source_empty_file) {
+	TestReporter r;
+
+	std::string path = std::filesystem::path(__FILE__).remove_filename().append("empty_file.co").string();
+	cero::Source source = cero::Source::from_file(path);
+	cero::build_source(source, r);
+}
+
+CERO_TEST(build_source_non_empty_file) {
+	TestReporter r;
+
+	std::string path = std::filesystem::path(__FILE__).remove_filename().append("non_empty_file.co").string();
+	cero::Source source = cero::Source::from_file(path);
+	cero::build_source(source, r);
+}
+
+} // namespace tests
