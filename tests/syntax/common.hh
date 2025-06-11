b5a2cdef90eebc1ecda6129682ed659cc7a9e058
Hannes Harnisch <hannes.harnisch@gmx.net>
2025-06-12 00:59:28 +0200

    Implement punctuation and operators in lexer

--- tests/syntax/common.hh
+++ tests/syntax/common.hh
@@ -1,9 +1,9 @@
 ﻿#pragma once
 
-#include <cero/syntax/token_list.hh>
+#include <cero/syntax/token.hh>
 
 namespace tests {
 
-void check_tokens_equal(const cero::TokenList& tokens, std::span<const cero::Token> expected_tokens);
+void check_tokens_equal(const char* source_text, std::span<const cero::Token> expected_tokens);
 
 }
