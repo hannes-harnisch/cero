e9a292f847712f64c7e86b036326a621e3dfa58a
Hannes Harnisch <hannes.harnisch@gmx.net>
2025-06-20 07:31:18 +0200

    Implement comments in lexer

--- cero/syntax/lexer.hpp
+++ cero/syntax/lexer.hpp
@@ -3,6 +3,7 @@
 #include "cero/report/reporter.hpp"
 #include "cero/syntax/source.hpp"
 #include "cero/syntax/token_list.hpp"
+#include "cero/util/flags.hpp"
 
 namespace cero {
 
@@ -10,6 +11,9 @@ enum class LexerFlags {
 	none = 0,
 	emit_comments = 1 << 0,
 };
+
+template<>
+struct is_flag_enum<LexerFlags> : std::true_type {};
 
 /// Performs lexical analysis on a single source input.
 TokenList run_lexer(const SourceView& source, Reporter& reporter, LexerFlags flags, uint8_t tab_size = default_tab_size);
