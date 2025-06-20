3b3b40344454a24c5c101a3f345ab34b454b91ef
Hannes Harnisch <hannes.harnisch@gmx.net>
2025-06-20 07:31:18 +0200

    Implement comments in lexer

--- cero/syntax/lexer.cpp
+++ cero/syntax/lexer.cpp
@@ -313,6 +313,7 @@ struct Lexer {
 
 	void on_left_angle() {
 		SourceSize offset = get_offset_and_advance();
+
 		if (cursor_.match('<')) {
 			if (cursor_.match('=')) {
 				tokens_.add(TokenKind::l_angle_angle_eq, offset, 0);
@@ -328,6 +329,7 @@ struct Lexer {
 
 	void on_right_angle() {
 		SourceSize offset = get_offset_and_advance();
+
 		if (cursor_.match('>')) {
 			if (cursor_.match('=')) {
 				tokens_.add(TokenKind::r_angle_angle_eq, offset, 0);
@@ -357,6 +359,7 @@ struct Lexer {
 
 	void on_colon() {
 		SourceSize offset = get_offset_and_advance();
+
 		if (cursor_.match(':')) {
 			tokens_.add(TokenKind::colon_colon, offset, 0);
 		} else {
@@ -366,6 +369,7 @@ struct Lexer {
 
 	void on_equals() {
 		SourceSize offset = get_offset_and_advance();
+
 		if (cursor_.match('=')) {
 			tokens_.add(TokenKind::eq_eq, offset, 0);
 		} else if (cursor_.match('>')) {
@@ -377,6 +381,7 @@ struct Lexer {
 
 	void on_plus() {
 		SourceSize offset = get_offset_and_advance();
+
 		if (cursor_.match('+')) {
 			tokens_.add(TokenKind::plus_plus, offset, 0);
 		} else if (cursor_.match('=')) {
@@ -388,6 +393,7 @@ struct Lexer {
 
 	void on_minus() {
 		SourceSize offset = get_offset_and_advance();
+
 		if (cursor_.match('>')) {
 			tokens_.add(TokenKind::thin_arrow, offset, 0);
 		} else if (cursor_.match('-')) {
@@ -401,6 +407,7 @@ struct Lexer {
 
 	void on_star() {
 		SourceSize offset = get_offset_and_advance();
+
 		if (cursor_.match('=')) {
 			tokens_.add(TokenKind::star_eq, offset, 0);
 		} else {
@@ -410,15 +417,60 @@ struct Lexer {
 
 	void on_slash() {
 		SourceSize offset = get_offset_and_advance();
-		if (cursor_.match('=')) {
+
+		if (cursor_.match('/')) {
+			consume_line_comment();
+
+			if (has_flags(flags_, LexerFlags::emit_comments)) {
+				SourceSize length = cursor_.offset() - offset;
+				tokens_.add(TokenKind::line_comment, offset, length);
+			}
+		} else if (cursor_.match('*')) {
+			consume_block_comment(offset);
+
+			if (has_flags(flags_, LexerFlags::emit_comments)) {
+				SourceSize length = cursor_.offset() - offset;
+				tokens_.add(TokenKind::block_comment, offset, length);
+			}
+		} else if (cursor_.match('=')) {
 			tokens_.add(TokenKind::slash_eq, offset, 0);
 		} else {
 			tokens_.add(TokenKind::slash, offset, 0);
 		}
 	}
 
+	void consume_line_comment() {
+		while (char c = cursor_.peek()) {
+			if (c == '\n') {
+				break;
+			}
+			cursor_.advance();
+		}
+	}
+
+	void consume_block_comment(SourceSize offset) {
+		uint32_t unclosed_count = 1;
+
+		while (cursor_.valid()) {
+			if (cursor_.match('*')) {
+				if (cursor_.match('/') && --unclosed_count == 0) {
+					return;
+				}
+			} else if (cursor_.match('/')) {
+				if (cursor_.match('*')) {
+					++unclosed_count;
+				}
+			} else {
+				cursor_.advance();
+			}
+		}
+
+		report(Message::unterminated_block_comment, offset, MessageArgs());
+	}
+
 	void on_percent() {
 		SourceSize offset = get_offset_and_advance();
+
 		if (cursor_.match('=')) {
 			tokens_.add(TokenKind::percent_eq, offset, 0);
 		} else {
@@ -428,6 +480,7 @@ struct Lexer {
 
 	void on_ampersand() {
 		SourceSize offset = get_offset_and_advance();
+
 		if (cursor_.match('&')) {
 			if (cursor_.match('=')) {
 				tokens_.add(TokenKind::and_and_eq, offset, 0);
@@ -443,6 +496,7 @@ struct Lexer {
 
 	void on_pipe() {
 		SourceSize offset = get_offset_and_advance();
+
 		if (cursor_.match('|')) {
 			if (cursor_.match('=')) {
 				tokens_.add(TokenKind::pipe_pipe_eq, offset, 0);
@@ -458,6 +512,7 @@ struct Lexer {
 
 	void on_tilde() {
 		SourceSize offset = get_offset_and_advance();
+
 		if (cursor_.match('=')) {
 			tokens_.add(TokenKind::tilde_eq, offset, 0);
 		} else {
@@ -467,6 +522,7 @@ struct Lexer {
 
 	void on_bang() {
 		SourceSize offset = get_offset_and_advance();
+
 		if (cursor_.match('=')) {
 			tokens_.add(TokenKind::bang_eq, offset, 0);
 		} else {
