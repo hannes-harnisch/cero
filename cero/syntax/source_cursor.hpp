1752a5cbf5e7cff7b9dd37cf49b2488dacc73acc
Hannes Harnisch <hannes.harnisch@gmx.net>
2025-06-23 04:47:10 +0200

    Add Unicode support for identifiers

--- cero/syntax/source_cursor.hpp
+++ cero/syntax/source_cursor.hpp
@@ -24,6 +24,16 @@ public:
 		}
 	}
 
+	/// Returns the current character and advances or null if the cursor is at the end.
+	char next() {
+		if (it_ != end_) {
+			return *it_++;
+		}
+		else {
+			return '\0';
+		}
+	}
+
 	/// Moves the cursor to the next character.
 	void advance() {
 		if (it_ != end_) {
