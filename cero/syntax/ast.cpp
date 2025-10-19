1333d626c68970bb8f7b6ae8e56871601781a01e
Hannes Harnisch <hannes.harnisch@gmx.net>
2025-10-19 13:58:42 +0200

    Begin parser

---
+++ cero/syntax/ast.cpp
@@ -0,0 +1,10 @@
+#include "ast.hpp"
+
+namespace cero {
+
+Ast::Ast(const TokenList& token_list) {
+	// TODO: initialize size of allocator based on token list
+	(void) token_list;
+}
+
+} // namespace cero
